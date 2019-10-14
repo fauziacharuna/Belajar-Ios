40a3890e1125b4a94c70153eb9b3dc1be250792a
fauziachmadharuna <fauziharuna@gmail.com>
2019-10-14 13:15:03 +0700

    Initial Commit

---
+++ Belajar IosTests/Belajar_IosTests.swift
@@ -0,0 +1,34 @@
+//
+//  Belajar_IosTests.swift
+//  Belajar IosTests
+//
+//  Created by Fauzi Achmad Haruna on 14/10/19.
+//  Copyright © 2019 Fauzi Achmad Haruna. All rights reserved.
+//
+
+import XCTest
+@testable import Belajar_Ios
+
+class Belajar_IosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
