0ce6981ea798b7049f74eb56f5a3f35aac90fa49
Muhammad Rifqi Fatchurrahman <muh_rif@live.com>
2019-11-23 13:30:07 +0700

    hapus code yg tidak perlu, tambah constraint tableview di interface builder, ubah susunan code

--- Belajar Ios/view/ContactTableViewController.swift
+++ Belajar Ios/view/ContactTableViewController.swift
@@ -17,57 +17,6 @@ class ContactTableViewController: UIViewController, UITableViewDataSource, UITab
     let cellCustom = "customCell"
     var contacts: [ContactGroup] = []
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contacts.count
-    }
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return self.contacts.count
-    }
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return self.contacts[section].section
-    }
-    
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: ContactCell = self.tableView.dequeueReusableCell(withIdentifier: cellCustom, for: indexPath) as! ContactCell
-        
-        cell.myCellLabel?.text = contacts[indexPath.section].items[indexPath.row].name
-        cell.statusLabel?.text = contacts[indexPath.section].items[indexPath.row].role
-        
-        let imageCell = UIImage(named: "apple")
-        cell.myView.layer.cornerRadius = 20
-        cell.myView.image = imageCell
-        
-        cell.layoutMargins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-        
-        return cell
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("You tap cell \(indexPath.row)")
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
-    }
-    
-    
-    override func loadView() {
-        super.loadView()
-        let tableView = UITableView(frame: .zero, style: .plain)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(tableView)
-        NSLayoutConstraint.activate([self.view.safeAreaLayoutGuide.topAnchor.constraint(equalTo: tableView.topAnchor),
-                                     self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: tableView.bottomAnchor),
-                                     self.view.leadingAnchor.constraint(equalTo: tableView.leadingAnchor),
-                                     self.view.trailingAnchor.constraint(equalTo: tableView.trailingAnchor)
-            
-        ])
-        self.tableView = tableView
-        
-    }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         //section A
@@ -89,24 +38,46 @@ class ContactTableViewController: UIViewController, UITableViewDataSource, UITab
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UINib(nibName: "ContactCell", bundle: nil), forCellReuseIdentifier: cellCustom)
-        self.view.addSubview(tableView)
-        setupAutoLayout()
-        
-      
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.contacts[section].section
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: ContactCell = self.tableView.dequeueReusableCell(withIdentifier: cellCustom, for: indexPath) as! ContactCell
+        
+        cell.myCellLabel?.text = contacts[indexPath.section].items[indexPath.row].name
+        cell.statusLabel?.text = contacts[indexPath.section].items[indexPath.row].role
+        
+        let imageCell = UIImage(named: "apple")
+        cell.myView.layer.cornerRadius = 20
+        cell.myView.image = imageCell
+        
+        cell.layoutMargins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tap cell \(indexPath.row)")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
     func addTapped(sender:UIBarButtonItem){
         print("Add trigger")
     }
-    
-    
-    func setupAutoLayout() {
-        
-        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-    }
-    
     
     @IBAction func backButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
